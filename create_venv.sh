e570ab477b4f2b4053c7683243e0d43fdeb26ac9
Brad Rowe <gord1anknot@users.noreply.github.com>
2015-02-08 21:53:47 -0700

    introduce dev-requirements.txt

--- create_venv.sh
+++ create_venv.sh
@@ -4,7 +4,7 @@ if [ ! -d ".venv" ] ; then
     if [ -f "$(which virtualenv)" ] ; then
         virtualenv --python=python2.7 --prompt=\(pyfitnesspal\) .venv
         .venv/bin/pip install -r requirements.txt
-        .venv/bin/pip install honcho
+        .venv/bin/pip install -r dev-requirements.txt
     else
         echo "Please ensure that virtualenv is installed and on the PATH."
     fi
